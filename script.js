2419329eac5dc23bd495be674809e6b4beafaed8
Joseph Valle <josephrvalle@gmail.com>
2023-02-01 08:52:01 -0500

    end of work on January 31

--- script.js
+++ script.js
@@ -10,9 +10,69 @@ async function wikiFetch(URL) {
   try {
     const res = await fetch(URL);
     const jsonObject = await res.json();
-    console.log(jsonObject);
+
+    // Number of entries we want per content block.
+    const entriesNum = 5;
+
+    // Store the API responses as arrays in their individual categories.
+    const births = jsonObject.births;
+    const deaths = jsonObject.births;
+    const events = jsonObject.events;
+    const holidays = jsonObject.holidays;
+    const selected = jsonObject.selected;
+    getSelected(selected, entriesNum);
   } catch (error) {
     console.log(error);
   }
+}
+// Select random entries from the "selection" array returned by our API call.
+function getSelected(selected, entriesNum, selection = {}, selections = []) {
+  // Generate random numbers to use as indexes to pull from the "selection" array.
+  const randEntries = randomSelection(selected.length, entriesNum);
+  // Loop through our random array and pull out the entry at the random index.
+  while (randEntries.length > 0) {
+    let currSelection = selected[randEntries.pop()];
+    selections.push({ year: currSelection.year, description: currSelection.text });
+  }
+  // Sort the array by year in order to display the information chronologically.
+  const selectionsSorted = [];
+  let compYear = 0;
+  for (let i = 0; i < entriesNum; i++) {
+    if (selections[i].year > compYear) {
+      console.log(`${selections[i].year} is greater than ${compYear}`);
+      selectionsSorted.push(selections[i].year)
+      compYear = selections[i].year;
+      console.log(selectionsSorted);
+    }
+    else {
+      console.log(`${selections[i].year} is less than ${compYear}`);
+      for (let j = 0; j < selectionsSorted.length; j++) {
+        console.log(selections[i].year, selectionsSorted[j]);
+      }
+
+    }
+
+
+    // if (selectionsSorted.length === 0) {
+    //   selectionsSorted.push(selections[i]);
+    //   console.log(selections[i].year);
+    // }
+    // else {
+    //   if (selections[i].year )
+    // }
+  }
+  console.log(selectionsSorted);
+}
+
+// Create and return an array of random numbers based off the number of entries that were returned. Limit the amount of 
+// random numbers by the number of desired entries.
+function randomSelection(upperLimit, entriesNum, randArr = []) {
+  while (randArr.length < entriesNum) {
+    let randNum = Math.floor(Math.random() * upperLimit);
+    if (!randArr.includes(randNum)) {
+      randArr.push(randNum);
+    }
+  }
+  return (randArr);
 }
 
