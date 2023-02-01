8c75f06402091b920d3286500b45f2cf8f5d779f
Joseph Valle <josephrvalle@gmail.com>
2023-02-01 16:07:25 -0500

    added a few features, found a workaround for the CSS issue using the .hided class

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
+
+// Declare our global variables.
 const today = new Date();
-const day = String(today.getDate()).padStart(2, '0');
+const day = String(today.getDate());
 const month = String(today.getMonth() + 1).padStart(2, '0');
 const monthArr = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const year = today.getFullYear();
-const BASE_URL = `https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all/${month}/${day}`;
+const wikiAllURL = `https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all/${month}/${day}`;
+const wikiFeaturedURL = `https://api.wikimedia.org/feed/v1/wikipedia/en/featured/${year}/${month}/${String(today.getDate()).padStart(2, '0')}`;
+const entriesNum = 5;
 
-wikiFetch(BASE_URL);
+// Call the functions to set up the page.
+setUpNavBar(day, month, monthArr, year); // Gets and sets the content for the navigation bar.
+wikiAllFetch(wikiAllURL); // Gets and sets the content for selected articles, births, deaths, and holidays.
+wikiFeaturedFetch(wikiFeaturedURL); // Gets and sets the content for today's featured article.
 
 function setUpNavBar(day, month, monthArr, year) {
-  const nav = document.getElementsByTagName("nav");
+  // Set the date at left.
+  const nav = document.querySelector("nav");
   const todayIs = document.getElementById("today");
-  todayIs.innerText = `Today is ${monthArr[month - 1]} ${day}, ${year}.`;
-  nav.append(todayIs);
+  todayIs.innerText = `${monthArr[month - 1]} ${day}, ${year}`;
+  // Start the clock.
+  startTime();
+  // Get IP address and location.
+  getIP();
 }
 
-
-
-// Asynchronous function that gets our data from the Wikipedia API.
-async function wikiFetch(URL) {
+// Async function that gets and sets our data from the daily Wikipedia landing page.
+async function wikiAllFetch(URL) {
   try {
     const res = await fetch(URL);
     const jsonObject = await res.json();
-    // Number of entries we want per content block.
-    const entriesNum = 5;
     // Store the API responses as arrays in their individual categories.
     const births = jsonObject.births;
     const deaths = jsonObject.births;
@@ -30,6 +37,24 @@ async function wikiFetch(URL) {
     const holidays = jsonObject.holidays;
     const selected = jsonObject.selected;
     getSelected(selected, entriesNum);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Async function that gets and sets our date from Wikipedia's featured article of the day.
+async function wikiFeaturedFetch(URL) {
+  try {
+    const res = await fetch(URL);
+    const jsonObject = await res.json();
+    // Number of entries we want per content block.
+    const entriesNum = 5;
+    // Store the API responses as arrays in their individual categories.
+    const featuredArticle = jsonObject.tfa;
+    const mostRead = jsonObject.mostread;
+    const news = jsonObject.news;
+    setFeaturedArticle(featuredArticle);
+    getNews(news, entriesNum);
   } catch (error) {
     console.log(error);
   }
@@ -65,11 +90,114 @@ function randomSelection(upperLimit, entriesNum, randArr = []) {
 
 // Add our selections to the DOM.
 function selectionsToDOM(selections) {
+  const containerDiv = document.getElementById("grid-left");
   const addDiv = document.createElement("div");
+  addDiv.classList.add("grid-box");
   for (const selection of selections) {
     let addP = document.createElement("p");
     addP.innerHTML = `<span class="date">${selection.year}</span> ${selection.description}`;
     addDiv.append(addP);
   }
-  document.body.append(addDiv);
+  containerDiv.append(addDiv);
+}
+
+// Add featured article to the DOM.
+function setFeaturedArticle(article) {
+  const gridBox = document.createElement("div");
+  gridBox.classList.add("grid-box");
+  gridBox.id = article.normalizedtitle;
+  const img = document.createElement("img");
+  img.src = article.originalimage.source;
+  gridBox.append(img);
+  const gridRight = document.getElementById("grid-right");
+  gridRight.append(gridBox);
+  const test = document.createElement("div");
+  test.classList.add("grid-box");
+  test.classList.add("hided");
+
+  test.innerHTML = "<p>hi</p>";
+  gridRight.append(test);
+}
+
+// Get the information we want from the news pull of the API.
+function getNews(articles, entriesNum, newsArray = []) {
+  for (let i = 0; i < entriesNum; i++) {
+    let description = articles[i].links[0].extract;
+    let link = articles[i].links[0].content_urls.desktop.page;
+    newsArray.push({ link: link, description: description });
+  }
+  setNews(newsArray);
+}
+
+// Add the Wikipedia news to the DOM.
+function setNews(news) {
+  // Add our selections to the DOM.
+  const containerDiv = document.getElementById("grid-center");
+  const addDiv = document.createElement("div");
+  addDiv.classList.add("grid-box");
+  for (const article of news) {
+    let p = document.createElement("p");
+    p.innerHTML = `${article.description}<br /><a href="${article.link}" target="_blank">${article.link}</a>`;
+    addDiv.append(p);
+  }
+  containerDiv.append(addDiv);
+}
+
+// Copy and paste clock code from W3Schools : )
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById("time").innerText = h + ":" + m + ":" + s;
+  setTimeout(startTime, 1000);
+}
+
+// Update the clock every second.
+function checkTime(i) {
+  if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+  return i;
+}
+
+// Get users IP address and pass it to a different API to get their physical location.
+async function getIP() {
+  const URL = "https://api.ipify.org/?format=json";
+  const URL2 = "https://ipgeolocation.abstractapi.com/v1/?api_key=44050c553ac64009841a277fe660358f&ip_address="
+  try {
+    const res = await fetch(URL);
+    const jsonObject = await res.json();
+    getLocation(URL2 + jsonObject.ip);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Get users physical location.
+async function getLocation(URL) {
+  try {
+    const res = await fetch(URL);
+    const jsonObject = await res.json();
+    const city = jsonObject.city;
+    const state = jsonObject.region_iso_code;
+    const zipCode = jsonObject.postal_code;
+    const country = jsonObject.country_code;
+    const flagEmoji = jsonObject.flag.emoji;
+    setLocation(city, state, zipCode, country, flagEmoji);
+    getMap(jsonObject.latitude, jsonObject.longitude);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Set users physical location in the navigation bar.
+function setLocation(city, state, zipCode, country, flagEmoji) {
+  const location = document.getElementById("location");
+  location.innerText = `${city}, ${state} ${zipCode}`;
+}
+
+// Try to use GoogleMaps API to get a map?
+function getMap(lat, long) {
+  console.log(lat, long);
 }
